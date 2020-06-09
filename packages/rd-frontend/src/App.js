0b2ddc8b821365e165cdba41e52b7e835fb4c015
Victor Song <vms2@rice.edu>
2020-06-09 17:30:19 -0500

    New login flow being implemented

--- packages/rd-frontend/src/App.js
+++ packages/rd-frontend/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Login from "./components/Login";
+import Other from "./components/Other";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/login">
+                    <Login />
+                </Route>
+                <Route path="/other">
+                    <Other />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
