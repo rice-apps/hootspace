112ac0cdda98da28b20f539b725ed12b1c6a299a
Victor Song <vms2@rice.edu>
2020-06-12 02:57:43 -0500

    Started setting up Apollo Client with auth

--- packages/rd-frontend/src/components/Other.js
+++ packages/rd-frontend/src/components/Other.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { useQuery, useSubscription } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
 
 import OtherH1 from "./Other.styles";
 
+const SUB_USERNAME = gql`
+    {
+        profileUpdated {
+            username
+        }
+    }
+`;
+
 function Other() {
-    return <OtherH1>Another page inside the application</OtherH1>;
+    const { loading, error, data } = useSubscription(SUB_USERNAME);
+
+    console.log(data);
+
+    if (loading) return <h1>Loading...</h1>;
+    if (error) return <h1>Something went wrong...</h1>;
+
+    return <OtherH1>{data.profileUpdated.username}</OtherH1>;
 }
 
 export default Other;
