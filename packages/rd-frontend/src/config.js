3ce67b410d58a1d979b969480ce2b2fcd61f5d62
Victor Song <vms2@rice.edu>
2020-06-12 02:57:43 -0500

    Started setting up Apollo Client with auth

--- packages/rd-frontend/src/config.js
+++ packages/rd-frontend/src/config.js
@@ -2,5 +2,12 @@ const CAS_AUTH_URL = "https://idp.rice.edu/idp/profile/cas/login";
 const SERVICE_URL = "http://localhost:3000/login";
 const FRONTEND_AUTH_URL = CAS_AUTH_URL + "?service=" + SERVICE_URL;
 const BACKEND_AUTH_URL = "http://localhost:3001/login";
+const GQL_URL = "http://localhost:3001/graphql";
 
-export { CAS_AUTH_URL, SERVICE_URL, FRONTEND_AUTH_URL, BACKEND_AUTH_URL };
+export {
+    CAS_AUTH_URL,
+    SERVICE_URL,
+    FRONTEND_AUTH_URL,
+    BACKEND_AUTH_URL,
+    GQL_URL,
+};
