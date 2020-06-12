27541f98b37a8bfc745af4b5682098a78ee58710
Victor Song <vms2@rice.edu>
2020-06-12 02:57:43 -0500

    Started setting up Apollo Client with auth

--- packages/rd-frontend/src/index.js
+++ packages/rd-frontend/src/index.js
@@ -1,15 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { createHttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
 
 import GlobalStyle from "./index.styles";
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import { GQL_URL } from "./config";
+
+const httpLink = createHttpLink({
+    uri: GQL_URL,
+    credentials: "same-origin",
+});
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem("RD_TOKEN");
+
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? token : "",
+        },
+    };
+});
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: authLink.concat(httpLink),
+});
+
 ReactDOM.render(
     <React.StrictMode>
         <GlobalStyle />
-        <App />
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
     </React.StrictMode>,
     document.getElementById("root"),
 );
