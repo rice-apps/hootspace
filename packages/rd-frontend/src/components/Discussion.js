5b6295cccba842be7007629453f76b779b64a255
Shryans Goyal <shryansgoyal@gmail.com>
2020-06-12 21:09:58 -0500

    Kludgy way to get subscriptions and queries to work simultaneously

--- packages/rd-frontend/src/components/Discussion.js
+++ packages/rd-frontend/src/components/Discussion.js
@@ -1,13 +1,25 @@
 import React, { useEffect } from "react";
 
-import { DiscussionTitle } from "./Discussion.styles";
+import { DiscussionTitle, DiscussionBody } from "./Discussion.styles";
 
 function Discussion(props) {
     useEffect(() => {
         props.subscribeToNewDiscussion();
     });
 
-    return <DiscussionTitle>hi</DiscussionTitle>;
+    if (props.loading) return <h1>Loading...</h1>;
+    if (props.error) return <h1>Something went wrong...</h1>;
+
+    const discussions = props.data.discussionMany.map((post) => {
+        return (
+            <div key={Math.random() * 100}>
+                <DiscussionTitle>{post.title}</DiscussionTitle>
+                <DiscussionBody>{post.body}</DiscussionBody>
+            </div>
+        );
+    });
+
+    return <div>{discussions}</div>;
 }
 
 export default Discussion;
--- packages/rd-frontend/src/components/Discussion.styles.js
+++ packages/rd-frontend/src/components/Discussion.styles.js
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 
 const DiscussionTitle = styled.h1`
-    font-family: "Oxygen";
+    font-family: "Ubuntu";
     font-size: 4vh;
 `;
 
-export { DiscussionTitle };
+const DiscussionBody = styled.p`
+    font-family: "Ubuntu";
+`;
+export { DiscussionTitle, DiscussionBody };
