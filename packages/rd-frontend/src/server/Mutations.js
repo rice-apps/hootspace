0c14860afec8e3792f370bc5e3f18ef4dca25e84
Shryans Goyal <shryansgoyal@gmail.com>
2020-06-26 16:14:53 -0500

    Merge pull request #2 from rice-apps/henry
    Pulling frontend for posts with vote frontend included. @Tyrubias

---
+++ packages/rd-frontend/src/server/Mutations.js
@@ -0,0 +1,150 @@
+import gql from "graphql-tag";
+
+const CREATE_DISCUSSION = gql`
+    mutation CreateDiscussion(
+        $title: String!
+        $body: String!
+        $creator: String!
+    ) {
+        discussionCreateOne(
+            record: { title: $title, body: $body, creator: $creator }
+        ) {
+            record {
+                _id
+                title
+                creator {
+                    _id
+                    username
+                }
+                body
+                date_created
+                __typename
+            }
+        }
+    }
+`;
+
+const CREATE_EVENT = gql`
+    mutation CreateEvent(
+        $title: String!
+        $body: String!
+        $creator: String!
+        $start: Date!
+        $end: Date!
+        $place: String!
+    ) {
+        eventCreateOne(
+            record: {
+                title: $title
+                body: $body
+                creator: $creator
+                start: $start
+                end: $end
+                place: $place
+            }
+        ) {
+            record {
+                _id
+                title
+                creator {
+                    _id
+                    username
+                }
+                body
+                date_created
+                start
+                end
+                place
+                __typename
+            }
+        }
+    }
+`;
+
+const CREATE_JOB = gql`
+    mutation CreateJob(
+        $title: String!
+        $body: String!
+        $creator: String!
+        $start: Date!
+        $end: Date!
+        $place: String!
+        $isPaid: Boolean!
+        $isClosed: Boolean!
+    ) {
+        jobCreateOne(
+            record: {
+                title: $title
+                body: $body
+                creator: $creator
+                start: $start
+                end: $end
+                place: $place
+                isPaid: $isPaid
+                isClosed: $isClosed
+            }
+        ) {
+            record {
+                _id
+                title
+                creator {
+                    _id
+                    username
+                }
+                body
+                date_created
+                start
+                end
+                place
+                isPaid
+                isClosed
+                __typename
+            }
+        }
+    }
+`;
+
+const CREATE_NOTICE = gql`
+    mutation CreateJob(
+        $title: String!
+        $body: String!
+        $creator: String!
+        $deadline: Date!
+    ) {
+        noticeCreateOne(
+            record: {
+                title: $title
+                body: $body
+                creator: $creator
+                deadline: $deadline
+            }
+        ) {
+            record {
+                _id
+                title
+                creator {
+                    _id
+                    username
+                }
+                body
+                date_created
+                deadline
+                __typename
+            }
+        }
+    }
+`;
+
+const LOGIN = gql`
+    mutation Login($ticket: String!) {
+        userAuthentication(ticket: $ticket) {
+            username
+            netID
+            isNewUser
+            token
+            __typename
+        }
+    }
+`;
+
+export { CREATE_DISCUSSION, CREATE_EVENT, CREATE_JOB, CREATE_NOTICE, LOGIN };
