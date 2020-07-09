d7aa1d04a98bc116e01178919ac1c9b76495dcf9
Victor Song <vms2@rice.edu>
2020-07-09 17:32:22 -0500

    Merge pull request #4 from rice-apps/henry
    Added ability to upvote and downvotes

--- packages/rd-frontend/src/components/Discussion.js
+++ packages/rd-frontend/src/components/Discussion.js
@@ -6,21 +6,49 @@ import { makeStyles } from "@material-ui/core/styles";
 import IconButton from "@material-ui/core/IconButton";
 import ArrowDropUp from "@material-ui/icons/ArrowDropUp";
 import ArrowDropDown from "@material-ui/icons/ArrowDropDown";
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import ShareIcon from '@material-ui/icons/Share';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { useMutation } from "@apollo/react-hooks";
 
 import {
+    UPVOTE_POST,
+    DOWNVOTE_POST
+} from "../graphql/Mutations";
+
+import { TOKEN_NAME } from "../utils/config";
+
+import {
+    Banner,
     DiscussionBoxSection,
     DiscussionBox,
     LeftComponent,
     Likes,
     Upvote,
     Downvote,
-    Dislikes,
-    MiddleComponent,
+    TopComponent,
     DiscussionTitle,
+    TagOne,
+    TagTwo,
+    TagThree,
+    MiddleComponent,
     DiscussionBody,
     BottomComponent,
+    Save,
+    AddTo,
+    OP,
+    Time,
+    Date,
+    ShareFacebook,
+    ShareTwitter,
+    Share
 } from "./Discussion.styles";
 
+
+        
 const useStyles = makeStyles((theme) => ({
     root: {
         "& > *": {
@@ -34,53 +62,109 @@ function Discussion(props) {
 
     const classes = useStyles();
 
+    const userInfo = JSON.parse(localStorage.getItem(TOKEN_NAME));
+
+    const [upvotePost] = useMutation(UPVOTE_POST);
+
+    const [downvotePost] = useMutation(DOWNVOTE_POST);
+
     useEffect(() => {
         props.subscribeToNewPosts();
+        props.subscribeToNewVotes();
     }, []);
-
-    console.log(props.error)
 
     if (props.loading) return <h1>Loading...</h1>;
     if (props.error) return <h1>Something went wrong...</h1>;
 
     const discussions = props.data.postPagination.items.map((post, i) => {
-        console.log(post);
         return (
             <React.Fragment key={i}>
                 <DiscussionBoxSection>
-                    {post.creator.username}
                     <DiscussionBox>
                         <LeftComponent>
-                            <Likes>15</Likes>
                             <Upvote className={classes.root}>
-                                <IconButton>
+                                <IconButton onClick={(e) => {
+                                    e.preventDefault();
+                                    upvotePost({
+                                        variables: {
+                                            netID: userInfo.netID,
+                                            _id: post._id,
+                                        },
+                                    });
+                                }}>
+                                    
                                     <ArrowDropUp />
                                 </IconButton>
                             </Upvote>
+                            <Likes>{post.upvotes.length - post.downvotes.length}</Likes>
                             <Downvote className={classes.root}>
-                                <IconButton>
+                                <IconButton onClick={(e) => {
+                                    e.preventDefault();
+                                    downvotePost({
+                                        variables: {
+                                            netID: userInfo.netID,
+                                            _id: post._id,
+                                        },
+                                    });
+                                }}>
                                     <ArrowDropDown />
                                 </IconButton>
                             </Downvote>
-                            <Dislikes>3</Dislikes>
                         </LeftComponent>
 
-                        <MiddleComponent>
+                        <TopComponent>
                             <DiscussionTitle>{post.title}</DiscussionTitle>
+                            <TagOne>Tag 1</TagOne>
+                            <TagTwo>Tag 2</TagTwo>
+                            <TagThree>Tag 3</TagThree>
+                        </TopComponent>
+
+                        <MiddleComponent>
                             <DiscussionBody>
                                 {ReactHtmlParser(post.body)}
                             </DiscussionBody>
                         </MiddleComponent>
 
-                        <BottomComponent></BottomComponent>
+                        <BottomComponent>
+                            <Save>Save</Save>
+                            <AddTo>+ Add to...</AddTo>
+                            <OP>
+                                {post.creator.username}
+                            </OP>
+                            <Time>{post.date_created.substring(11, 16)}</Time>
+                            <Date>
+                                {post.date_created.substring(5, 7) + '/' + 
+                                post.date_created.substring(8, 10) + '/' + 
+                                post.date_created.substring(0, 4)}
+                            </Date>
+                            <ShareFacebook>
+                                <IconButton>
+                                    <FacebookIcon />
+                                </IconButton>
+                            </ShareFacebook>
+                            <ShareTwitter>
+                                <IconButton>
+                                    <TwitterIcon />
+                                </IconButton>
+                            </ShareTwitter>
+                            <Share>
+                                <IconButton>
+                                    <ShareIcon />
+                                </IconButton>
+                            </Share>
+                        </BottomComponent>
+
                     </DiscussionBox>
                 </DiscussionBoxSection>
             </React.Fragment>
+            
+            
         );
     });
 
     return (
         <React.Fragment>
+            <Banner />
             {discussions}
             <button
                 onClick={() => {
