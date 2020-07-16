5e4891466c442186e9fdda625511833d8db9c2a7
Victor Song <vms2@rice.edu>
2020-07-15 23:58:40 -0500

    Fixed props issue with styled components

--- packages/rd-frontend/src/pages/MoreInfo.styles.js
+++ packages/rd-frontend/src/pages/MoreInfo.styles.js
@@ -162,8 +162,8 @@ const ArrowI = styled.i`
     margin: 2.5px;
     margin-left: 13px;
 
-    transform: ${props.open ? "rotate(-135deg)" : "rotate(45deg)"};
-    -webkit-transform: ${props.open ? "rotate(-135deg)" : "rotate(45deg)"};
+    transform: ${props => props.open ? "rotate(-135deg)" : "rotate(45deg)"};
+    -webkit-transform: ${props => props.open ? "rotate(-135deg)" : "rotate(45deg)"};
 `;
 
 export {
