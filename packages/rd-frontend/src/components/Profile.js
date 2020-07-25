4dd9d2b38749ccc720e078d771bbb8838d5b726f
Victor Song <vms2@rice.edu>
2020-07-25 00:18:42 -0500

    Merge pull request #11 from rice-apps/shreya2
    Image Upload to Posts + Merging Components into one folder

--- packages/rd-frontend/src/components/Profile.js
+++ packages/rd-frontend/src/components/Profile.js
@@ -4,7 +4,7 @@ import { useMutation, useQuery, useLazyQuery } from "@apollo/client";
 import { SET_INFO } from "../graphql/Mutations";
 import { GET_USER_DATA } from "../graphql/Queries";
 import { TOKEN_NAME } from "../utils/config";
-import DropDownItem from "../components/DropDownItem.js";
+import DropDownItem from "./DropDownItem.js";
 import major_minor_json from "../utils/MajorMinor.json";
 import {
     DDWrapper,
