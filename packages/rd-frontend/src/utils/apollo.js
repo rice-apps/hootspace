50f0991637f987fbef43ce5ae20a121b2d4acc8e
Victor Song <vms2@rice.edu>
2020-08-14 18:06:58 -0500

    Minor formatting changes

--- packages/rd-frontend/src/utils/apollo.js
+++ packages/rd-frontend/src/utils/apollo.js
@@ -58,12 +58,12 @@ const mainClient = new ApolloClient({
         fields: {
           postConnection: relayStylePagination(),
           currentUser: {
-            read() {
+            read () {
               return currentUser()
             }
           },
           currentNetID: {
-            read() {
+            read () {
               return currentUser().netID
             }
           }
