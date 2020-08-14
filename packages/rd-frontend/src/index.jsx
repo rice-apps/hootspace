297df1156ded46500f99c5034306b27aef464fa5
Victor Song <vms2@rice.edu>
2020-08-14 02:34:35 -0500

    Removed generic comments

--- packages/rd-frontend/src/index.jsx
+++ packages/rd-frontend/src/index.jsx
@@ -21,9 +21,6 @@ render(
   document.getElementById('root')
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   serviceWorker.unregister()
 } else {
