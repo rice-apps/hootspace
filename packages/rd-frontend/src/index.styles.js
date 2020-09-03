efe53d2b23630879a7b5849547fbaa774edbf1c2
Victor Song <vms2@rice.edu>
2020-09-03 01:12:57 -0500

    Bundle local copy of Avenir Next

--- packages/rd-frontend/src/index.styles.js
+++ packages/rd-frontend/src/index.styles.js
@@ -1,17 +1,20 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: "Avenir";
+    src: 
+      local("Avenir") 
+      url("./fonts/AvenirLTStd-Book.otf") 
+      format("opentype");
+  }
   html {
     overflow-x: hidden;
   }
 
   body {
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
-      "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
-      "Helvetica Neue", sans-serif;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
+    font-family: "Avenir", sans-serif;
   }
 
   code {
